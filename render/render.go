27c99b2eba0971be6bd18a1737b3add38b98733b
Fuki Hayashi <knightf884@yahoo.co.jp>
2023-01-22 11:09:51 +0900

    google analyticsを使用する場合にテンプレートからタグを読み込むように変更

--- render/render.go
+++ render/render.go
@@ -3,6 +3,7 @@ package render
 import (
 	"html/template"
 	"log"
+	"os"
 	"path/filepath"
 	"strings"
 
@@ -19,13 +20,20 @@ func CreateRenderTemplates() multitemplate.Render {
 	}
 
 	htmls, err := filepath.Glob("./views/*.html")
+
+	analytics := os.Getenv("ANALYTICS")
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, html := range htmls {
 		slice := strings.Split(html, "/")
-		r.AddFromFilesFuncs(slice[len(slice)-1], template_funcs, "./views/layout.tmpl", "./views/_header.tmpl",
-			"./views/_footer.tmpl", "./views/_messages.tmpl", "./views/_search_form.tmpl", "./views/_tag_accordion.tmpl", html)
+		if analytics == "yes" {
+			r.AddFromFilesFuncs(slice[len(slice)-1], template_funcs, "./views/layout.tmpl", "./views/_header.tmpl",
+				"./views/_footer.tmpl", "./views/_messages.tmpl", "./views/_search_form.tmpl", "./views/_tag_accordion.tmpl", "./views/_google_analytics.tmpl", html)
+		} else {
+			r.AddFromFilesFuncs(slice[len(slice)-1], template_funcs, "./views/layout.tmpl", "./views/_header.tmpl",
+				"./views/_footer.tmpl", "./views/_messages.tmpl", "./views/_search_form.tmpl", "./views/_tag_accordion.tmpl", html)
+		}
 	}
 	return r
 }
